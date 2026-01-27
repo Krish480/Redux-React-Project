1d93a6558001e3a7bf97a87287c2783d79105e5d
Krishna Pandey <krishnapandey18866@gmail.com>
2026-01-27 16:47:33 +0530

    Refactor ResultCard for photo gif and video

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const ResultCard = () => {
+const ResultCard = ({ item }) => {
   return (
-    <div>ResultCard</div>
+    <div>
+      {item.type == "photo" ? <img
+        key={item.id}
+        src={item.thumbnail}
+        alt={item.title}
+        className="w-full h-70 object-cover rounded-lg"
+      /> : ""}
+
+      {item.type == "gif" ? <img
+        key={item.id}
+        src={item.thumbnail}
+        alt={item.title}
+        className="w-full h-64 object-cover rounded-lg"
+      /> : ""}
+
+      {item.type == "video" ? <video 
+          autoPlay 
+          loop
+          muted
+          key={item.id}
+          src={item.src}
+          alt={item.title}
+          className="w-full h-64 object-cover rounded-lg"
+        /> : ""}
+
+    </div>
   )
 }
 
