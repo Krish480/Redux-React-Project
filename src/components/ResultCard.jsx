834b937ac82c0bc934ce77d4c2bc019d0f05dab6
Krishna Pandey <krishnapandey18866@gmail.com>
2026-01-28 16:25:21 +0530

    Add lazy loading for images and videos in ResultCard

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,32 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const ResultCard = ({ item }) => {
+  const videoRf = useRef(null)
+  const [play, setPlay] = useState(false)
+
+  useEffect(() => {
+    if(item.type !== "video") return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if(entry.isIntersecting){
+          setPlay(true)
+        } else {
+          setPlay(false);
+        }
+      },
+      {threshold: 0.5}
+    );
+
+    if(videoRf.current) observer.observe(videoRf.current);
+
+    return () => observer.disconnect();
+  },[item.type])
+
   return (
-    <div>
-      {item.type == "photo" ? <img
-        key={item.id}
-        src={item.thumbnail}
-        alt={item.title}
-        className="w-full h-70 object-cover rounded-lg"
-      /> : ""}
 
-      {item.type == "gif" ? <img
-        key={item.id}
-        src={item.thumbnail}
-        alt={item.title}
-        className="w-full h-64 object-cover rounded-lg"
-      /> : ""}
+    <div className=' w-full relative h-80 bg-white rounded'>
+      <a href={item.url} target='_blank' rel='noreferrer'>
+        <div className='h-full'>
+          {item.type == "photo" ? <img
+            loading='lazy'
+            key={item.id}
+            src={item.thumbnail}
+            alt={item.title}
+            className="w-full h-full object-cover object-center rounded"
+          /> : ""}
 
-      {item.type == "video" ? <video 
-          autoPlay 
-          loop
-          muted
-          key={item.id}
-          src={item.src}
-          alt={item.title}
-          className="w-full h-64 object-cover rounded-lg"
-        /> : ""}
+          {item.type == "gif" ? <img
+            key={item.id}
+            src={item.thumbnail}
+            alt={item.title}
+            className="w-full h-full object-cover object-center rounded"
+          /> : ""}
 
+          {item.type == "video" ? <video
+            ref={videoRf}
+            autoPlay = {play}
+            loop
+            muted
+            playsInline
+            key={item.id}
+            src={item.src}
+            alt={item.title}
+            className="w-full h-full object-cover object-center rounded"
+          /> : ""}
+
+        </div>
+      </a>
+
+      <div id='title' className='absolute bottom-0 flex justify-between items-center p-4 w-full px-6 py-10'>
+        <h2 className='text-lg font-semibold capitalize'>{item.title}</h2>
+        <button className='text-lg bg-blue-600 px-3 py-2 cursor-pointer rounded'><i class="fa-regular fa-bookmark"></i></button>
+      </div>
     </div>
   )
 }
